f22c62051663d1f212efb212af827c49bc289c39
lokideos <archmage09@gmail.com>
2018-10-08 01:45:44 +0300

    Change locales and app language to russian (probably)

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,8 @@
-require_relative 'boot'
+# frozen_string_literal: true
 
-require 'rails/all'
+require_relative "boot"
+
+require "rails/all"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -15,5 +17,6 @@ module TestGuru
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    config.i18n.default_locale = :ru
   end
 end

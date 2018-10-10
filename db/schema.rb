04e8bcfbdb0e1b97ddd00d9d7664ebe3c0d53cd0
Lokideos <archmage09@gmail.com>
2018-10-10 04:40:49 +0300

    Assign default value to tests' level

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_10_012951) do
+ActiveRecord::Schema.define(version: 2018_10_10_013920) do
 
   create_table "answers", force: :cascade do |t|
     t.string "body", null: false
@@ -37,7 +37,7 @@ ActiveRecord::Schema.define(version: 2018_10_10_012951) do
 
   create_table "tests", force: :cascade do |t|
     t.string "title", null: false
-    t.integer "level", null: false
+    t.integer "level", default: 0, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "category_id"

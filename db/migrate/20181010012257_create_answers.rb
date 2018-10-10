b21be93ff846820964755f60b30ecf8de37e6d6a
Lokideos <archmage09@gmail.com>
2018-10-10 04:24:34 +0300

    Answer model

---
+++ db/migrate/20181010012257_create_answers.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+class CreateAnswers < ActiveRecord::Migration[5.2]
+  def change
+    create_table :answers do |t|
+      t.string :body
+      t.boolean :correct
+      t.references :question, index: true, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_181_010_012_041) do
+ActiveRecord::Schema.define(version: 20_181_010_012_257) do
+  create_table 'answers', force: :cascade do |t|
+    t.string 'body'
+    t.boolean 'correct'
+    t.integer 'question_id'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.index ['question_id'], name: 'index_answers_on_question_id'
+  end
+
   create_table 'categories', force: :cascade do |t|
     t.string 'title'
     t.datetime 'created_at', null: false

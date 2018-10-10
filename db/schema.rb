441a6981390ffa804198a0daabb34dc20e06d203
Lokideos <archmage09@gmail.com>
2018-10-10 04:22:29 +0300

    Question model

--- db/schema.rb
+++ db/schema.rb
@@ -12,11 +12,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_181_010_011_848) do
+ActiveRecord::Schema.define(version: 20_181_010_012_041) do
   create_table 'categories', force: :cascade do |t|
     t.string 'title'
     t.datetime 'created_at', null: false
     t.datetime 'updated_at', null: false
+  end
+
+  create_table 'questions', force: :cascade do |t|
+    t.string 'body'
+    t.integer 'test_id'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.index ['test_id'], name: 'index_questions_on_test_id'
   end
 
   create_table 'tests', force: :cascade do |t|

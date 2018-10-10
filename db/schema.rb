40b3b5fc2ac4a1031bf6721c6be6bc79e289bd0e
Lokideos <archmage09@gmail.com>
2018-10-10 04:20:14 +0300

    Add reference to category for test

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_181_010_010_058) do
+ActiveRecord::Schema.define(version: 20_181_010_011_848) do
   create_table 'categories', force: :cascade do |t|
     t.string 'title'
     t.datetime 'created_at', null: false
@@ -24,5 +24,7 @@ ActiveRecord::Schema.define(version: 20_181_010_010_058) do
     t.integer 'level'
     t.datetime 'created_at', null: false
     t.datetime 'updated_at', null: false
+    t.integer 'category_id'
+    t.index ['category_id'], name: 'index_tests_on_category_id'
   end
 end

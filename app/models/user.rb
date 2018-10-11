a8f9bb6b7e1069cd86f1243fef8cb5c5e032f993
Lokideos <archmage09@gmail.com>
2018-10-11 19:26:16 +0300

    Refactoring finding tests with designated level for user (now with validations queries)

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,10 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
+  has_many :test_progresses
   has_many :tests, through: :test_progresses
 
   def tests_with_level(level)
-    Test
-      .where('level = :level', level: level)
-      .where(id: TestProgress.where(user_id: id))
+    tests.where('level = :level', level: level)
   end
 end

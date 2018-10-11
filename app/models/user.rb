9376d95254139e558b4900309671e72054120fc9
Lokideos <archmage09@gmail.com>
2018-10-11 19:28:29 +0300

    Refactoring finding tests with designated level for user method

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,6 @@ class User < ApplicationRecord
   has_many :tests, through: :test_progresses
 
   def tests_with_level(level)
-    tests.where('level = :level', level: level)
+    tests.where(level: level)
   end
 end

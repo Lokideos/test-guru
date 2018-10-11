4cfac2e3d7193931e59b3f80aecdb7eb52d81c49
Lokideos <archmage09@gmail.com>
2018-10-11 15:48:56 +0300

    Simplify finding needed tests for user method according to mentor request

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,6 @@
 
 class User < ApplicationRecord
   def tests_with_level(level)
-    test_ids = Progress.where('user_id = :user_id', user_id: id).pluck(:id)
-    Test.where('level = :level', level: level).where(id: test_ids)
+    Test.where('level = :level', level: level).where(id: Progress.where(user_id: id))
   end
 end

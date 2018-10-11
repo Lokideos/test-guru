cabcdfde39f2aa8c35ebba7662776c43cba59476
Lokideos <archmage09@gmail.com>
2018-10-11 15:43:09 +0300

    Simplify seach test by category as per mentor request

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,11 +1,10 @@
 # frozen_string_literal: true
 
 class Test < ApplicationRecord
-  class << self
-    def with_category(category)
-      category_id = Category.where('title = :category', category: category).pluck(:id).first
-      where('category_id = :category_id',
-            category_id: category_id).order(created_at: :desc).pluck(:title)
-    end
+  def self.with_category(category)
+    where('category_id = :category_id', category_id:
+          Category.where('title = :category', category: category).first).order(
+            created_at: :desc
+          ).pluck(:title)
   end
 end

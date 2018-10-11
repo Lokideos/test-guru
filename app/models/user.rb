28c98820ddc6d397a3e9e19edda4b480d5d32c3c
Lokideos <archmage09@gmail.com>
2018-10-11 14:57:47 +0300

    User now can return all tests he participate in

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
+  def tests_with_level(level)
+    test_ids = Progress.where('user_id = :user_id', user_id: id).pluck(:id)
+    Test.where('level = :level', level: level).where(id: test_ids)
+  end
 end

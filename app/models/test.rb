cc8c7215a5a3e3f4dda01bd74adad80554daf5f7
Lokideos <archmage09@gmail.com>
2018-10-11 14:31:44 +0300

    Find all tests with designated category in desc order

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,4 +1,11 @@
 # frozen_string_literal: true
 
 class Test < ApplicationRecord
+  class << self
+    def with_category(category)
+      category_id = Category.where('title = :category', category: category).pluck(:id).first
+      where('category_id = :category_id',
+            category_id: category_id).order(created_at: :desc).pluck(:title)
+    end
+  end
 end

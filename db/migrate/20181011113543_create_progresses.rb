5a506ce603c94a8b0f69700174fb016f1bb5483c
Lokideos <archmage09@gmail.com>
2018-10-11 15:51:41 +0300

    Get rid of unique preference in progresses migration

--- db/migrate/20181011113543_create_progresses.rb
+++ db/migrate/20181011113543_create_progresses.rb
@@ -3,7 +3,7 @@
 class CreateProgresses < ActiveRecord::Migration[5.2]
   def change
     create_table :progresses do |t|
-      t.references :test, foreign_key: true, unique: true
+      t.references :test, foreign_key: true
       t.references :user, foreign_key: true
       t.string :status
 

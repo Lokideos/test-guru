eac207a7fd3a2b0dc3f6a36c0f2261c79c31be89
Lokideos <archmage09@gmail.com>
2018-10-11 15:50:42 +0300

    Refactoring finding tests with needed category

--- app/models/test.rb
+++ app/models/test.rb
@@ -2,9 +2,8 @@
 
 class Test < ApplicationRecord
   def self.with_category(category)
-    where('category_id = :category_id', category_id:
-          Category.where('title = :category', category: category).first).order(
-            created_at: :desc
-          ).pluck(:title)
+    where(category_id: Category.where('title = :category', category: category).first).order(
+      created_at: :desc
+    ).pluck(:title)
   end
 end

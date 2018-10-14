e7acd16f9b5d44bdd8a81e090230a9d957b151ed
Lokideos <archmage09@gmail.com>
2018-10-15 02:21:23 +0300

    Fix counting answers for question - now differs for updating and creating new questions

--- app/models/question.rb
+++ app/models/question.rb
@@ -8,11 +8,16 @@ class Question < ApplicationRecord
 
   validates :body, presence: true
 
-  validate :validate_answers_quantity
+  validate :validate_answers_quantity_max
+  validate :validate_answers_quantity_empty, on: :update
 
   private
 
-  def validate_answers_quantity
-    errors.add(:answers) if answers.size > MAX_ANSWERS || answers.empty?
+  def validate_answers_quantity_max
+    errors.add(:answers) if answers.size > MAX_ANSWERS
+  end
+
+  def validate_answers_quantity_empty
+    errors.add(:answers) if answers.empty?
   end
 end

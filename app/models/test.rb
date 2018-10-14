74e6ea1c703742dd2a41ef55e998262ab6173de8
lokideos <archmage09@gmail.com>
2018-10-14 20:35:50 +0300

    Pick tests with designated difficulty level

--- app/models/test.rb
+++ app/models/test.rb
@@ -7,6 +7,8 @@ class Test < ApplicationRecord
   belongs_to :category
   belongs_to :author, class_name: 'User', foreign_key: :author_id
 
+  scope :with_level, ->(level) { where(level: level) }
+
   def self.with_category(category)
     joins(:category).where(categories: { title: category }).order(created_at: :desc).pluck(:title)
   end

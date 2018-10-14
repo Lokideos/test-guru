c23899da14561ae0f4a4c2a831aea8f55795843b
lokideos <archmage09@gmail.com>
2018-10-14 20:39:27 +0300

    Pick tests with easy, normal or BRUTAL difficulty

--- app/models/test.rb
+++ app/models/test.rb
@@ -8,6 +8,9 @@ class Test < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: :author_id
 
   scope :with_level, ->(level) { where(level: level) }
+  scope :easy, -> { where(level: 0..1) }
+  scope :normal, -> { where(level: 2..4) }
+  scope :brutal, -> { where('level > ?', 5) }
 
   def self.with_category(category)
     joins(:category).where(categories: { title: category }).order(created_at: :desc).pluck(:title)

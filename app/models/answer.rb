aaf4e28092f07006374d4e58dc07b6777f87299b
Lokideos <archmage09@gmail.com>
2018-10-15 13:25:03 +0300

    Move validations to answer and modify checking for zero answers for question validation

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,9 +1,28 @@
 # frozen_string_literal: true
 
 class Answer < ApplicationRecord
+  before_destroy :validate_answers_quantity_for_question_empty
+
+  MAX_ANSWERS = 4
+
   belongs_to :question
 
   validates :body, presence: true
 
+  validate :validate_answers_quantity_for_question_max
+
   scope :correct, -> { where(correct: true) }
+
+  private
+
+  def validate_answers_quantity_for_question_max
+    errors.add(:question) if question.answers.size >= MAX_ANSWERS
+  end
+
+  def validate_answers_quantity_for_question_empty
+    return if (question.answers.size - 1).positive?
+
+    errors.add(:question)
+    throw :abort
+  end
 end
--- app/models/question.rb
+++ app/models/question.rb
@@ -1,23 +1,8 @@
 # frozen_string_literal: true
 
 class Question < ApplicationRecord
-  MAX_ANSWERS = 4
-
   has_many :answers
   belongs_to :test
 
   validates :body, presence: true
-
-  validate :validate_answers_quantity_max
-  validate :validate_answers_quantity_empty, on: :update
-
-  private
-
-  def validate_answers_quantity_max
-    errors.add(:answers) if answers.size > MAX_ANSWERS
-  end
-
-  def validate_answers_quantity_empty
-    errors.add(:answers) if answers.empty?
-  end
 end

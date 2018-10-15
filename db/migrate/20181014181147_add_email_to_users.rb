f5ec01f470f2b30e400f33b431320bc8227e4abd
Lokideos <archmage09@gmail.com>
2018-10-15 12:32:13 +0300

    Change adding email to users migration name

--- db/migrate/20181014181147_add_email_to_users.rb
+++ db/migrate/20181014181147_add_email_to_users.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class ChangeEmailToUsers < ActiveRecord::Migration[5.2]
+class AddEmailToUsers < ActiveRecord::Migration[5.2]
   def change
     add_column :users, :email, :string
     change_column :users, :email, :string, null: false

c5b72380c946e555aadce704b55cbb52caa67f84
Lokideos <archmage09@gmail.com>
2018-10-18 01:20:15 +0300

    Redirecto from questions' index to related test

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -8,7 +8,7 @@ class QuestionsController < ApplicationController
   rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_question_not_found
 
   def index
-    @questions = @test.questions
+    redirect_to test_path(@test)
   end
 
   def show; end

9a7f2ae3e534cdded65206f6794b800a12aaa004
Lokideos <archmage09@gmail.com>
2018-10-19 12:15:44 +0300

    Delete redundant index action for Questions

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -6,10 +6,6 @@ class QuestionsController < ApplicationController
 
   rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_question_not_found
 
-  def index
-    redirect_to test_path(@test)
-  end
-
   def show; end
 
   def new

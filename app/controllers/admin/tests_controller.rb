1fd08ddc22213ea525302e0795586503d984db81
Lokideos <archmage09@gmail.com>
2018-10-25 23:22:44 +0300

    Delete redundant params permission for tests

--- app/controllers/admin/tests_controller.rb
+++ app/controllers/admin/tests_controller.rb
@@ -48,7 +48,7 @@ class Admin::TestsController < Admin::BaseController
   private
 
   def test_params
-    params.require(:test).permit(:title, :level, :category_id, :author_id)
+    params.require(:test).permit(:title, :level, :category_id)
   end
 
   def find_test

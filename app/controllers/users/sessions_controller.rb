2aaad871d594b453efdcf3145dae84df46e17b27
Lokideos <archmage09@gmail.com>
2018-10-26 00:15:16 +0300

    Add localization for devise and show user last & first name after login

---
+++ app/controllers/users/sessions_controller.rb
@@ -0,0 +1,34 @@
+# frozen_string_literal: true
+
+class Users::SessionsController < Devise::SessionsController
+  # before_action :configure_sign_in_params, only: [:create]
+
+  # GET /resource/sign_in
+  # def new
+  #   super
+  # end
+
+  # rubocop:disable Metrics/AbcSize
+  # POST /resource/sign_in
+  def create
+    self.resource = warden.authenticate!(auth_options)
+    set_flash_message!(:notice, :signed_in, first_name: @user.first_name,
+                                            last_name: @user.last_name)
+    sign_in(resource_name, resource)
+    yield resource if block_given?
+    respond_with resource, location: after_sign_in_path_for(resource)
+  end
+  # rubocop:enable Metrics/AbcSize
+
+  # DELETE /resource/sign_out
+  # def destroy
+  #   super
+  # end
+
+  # protected
+
+  # If you have extra params to permit, append them to the sanitizer.
+  # def configure_sign_in_params
+  #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
+  # end
+end

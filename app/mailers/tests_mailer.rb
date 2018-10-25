569f6fb02e944948298b769c7228e712742e920e
Lokideos <archmage09@gmail.com>
2018-10-25 21:37:38 +0300

    Add functionality for testing emails in dev env

---
+++ app/mailers/tests_mailer.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+class TestsMailer < ApplicationMailer
+  def completed_test(test_passage)
+    @user = test_passage.user
+    @test = test_passage.test
+
+    mail to: @user.email, subject: 'You just completed the TestGuru test!'
+  end
+end

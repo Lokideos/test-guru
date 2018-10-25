780f7c590bde95346587aff25d510e9fe5167f90
Lokideos <archmage09@gmail.com>
2018-10-25 23:40:56 +0300

    Redirect to managing tests if logged in user is an admin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,10 @@
 
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+
+  def after_sign_in_path_for(_user)
+    return admin_tests_path if current_user.is_a?(Admin)
+
+    tests_path
+  end
 end

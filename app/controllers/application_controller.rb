dae561a1f7a59255ff59f3586352583bcb2db853
Lokideos <archmage09@gmail.com>
2018-10-25 21:22:48 +0300

    Add basic devise-based authentication functionality

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,23 +2,4 @@
 
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
-
-  helper_method :current_user, :logged_in?
-
-  private
-
-  def authenticate_user!
-    return if current_user
-
-    cookies[:current_path] = request.original_url
-    redirect_to login_path, alert: 'Are you a Guru? Verify your Email and Password please'
-  end
-
-  def current_user
-    @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
-  end
-
-  def logged_in?
-    current_user.present?
-  end
 end
--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -18,6 +18,7 @@ class TestsController < ApplicationController
 
   def create
     @test = Test.new(test_params)
+    @test.author = current_user
 
     if @test.save
       redirect_to @test

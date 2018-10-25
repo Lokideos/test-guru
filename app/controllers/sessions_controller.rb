5f7ef972f1cdde32113d19ea29a5b4b06e9db9f9
lokideos <archmage09@gmail.com>
2018-10-25 08:12:07 +0300

    Show root page if no previous page was saved for user before log in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,7 +10,7 @@ class SessionsController < ApplicationController
 
     if user&.authenticate(params[:password])
       session[:user_id] = user.id
-      redirect_to after_login_url
+      redirect_to after_login_url || root_path
     else
       redirect_to login_path, alert: 'Are you a Guru? Verify your Email and Password please'
     end

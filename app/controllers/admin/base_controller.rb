082312782f20f8e782a32c5ce4554a58dd28c013
lokideos <archmage09@gmail.com>
2018-10-27 08:14:01 +0300

    Add method to check if user is admin

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -8,7 +8,7 @@ class Admin::BaseController < ApplicationController
   private
 
   def admin_required!
-    return if current_user.is_a?(Admin)
+    return if current_user.admin?
 
     redirect_to root_path, alert: 'You are not authorized to view this page.'
   end

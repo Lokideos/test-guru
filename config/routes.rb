0f66390573e567dcf58a46c849241e1ca7a65f4f
lokideos <archmage09@gmail.com>
2018-10-27 13:02:14 +0300

    Don't show default locale in url

--- config/routes.rb
+++ config/routes.rb
@@ -1,27 +1,30 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  get '/:lang' => 'tests#index'
   root to: 'tests#index'
 
-  devise_for :users, path: :gurus, path_names: { sign_in: :login, sign_out: :logout },
-                     controllers: { sessions: 'users/sessions' }
+  scope '(:lang)', lang: /en|ru/ do
+    devise_for :users, path: :gurus, path_names: { sign_in: :login, sign_out: :logout },
+                       controllers: { sessions: 'users/sessions' }
 
-  resources :tests, only: :index do
-    member do
-      post :start
+    resources :tests, only: :index do
+      member do
+        post :start
+      end
     end
-  end
 
-  resources :test_passages, only: %i[show update] do
-    member do
-      get :result
+    resources :test_passages, only: %i[show update] do
+      member do
+        get :result
+      end
     end
-  end
 
-  namespace :admin do
-    resources :tests do
-      resources :questions, shallow: true, except: [:index] do
-        resources :answers, shallow: true, except: [:index]
+    namespace :admin do
+      resources :tests do
+        resources :questions, shallow: true, except: [:index] do
+          resources :answers, shallow: true, except: [:index]
+        end
       end
     end
   end

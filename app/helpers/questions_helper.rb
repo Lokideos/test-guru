95eacd0c66fd2b093e2433bb8979c18b2f707dff
Lokideos <archmage09@gmail.com>
2018-10-31 02:09:21 +0300

    Questions administration stylization

--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -1,13 +1,23 @@
 # frozen_string_literal: true
 
 module QuestionsHelper
+  # rubocop:disable Metrics/MethodLength
+  # rubocop:disable Metrics/PerceivedComplexity
   def question_header(question)
-    test = question.test.title
-
-    if question.new_record?
-      "Create New #{test} Question"
-    else
-      "Edit #{test} Question"
+    if I18n.locale == :en
+      if question.new_record?
+        'New Question'
+      else
+        "Edit #{question.body}"
+      end
+    elsif I18n.locale == :ru
+      if question.new_record?
+        'Новый вопрос'
+      else
+        "Редактирование #{question.body}"
+      end
     end
   end
+  # rubocop:enable Metrics/MethodLength
+  # rubocop:enable Metrics/PerceivedComplexity
 end

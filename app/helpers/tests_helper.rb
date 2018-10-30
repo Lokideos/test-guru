5e98abe16517ece9717240675e3275b5ffbde157
Lokideos <archmage09@gmail.com>
2018-10-30 16:55:56 +0300

    Fix layout and tests page stilization

--- app/helpers/tests_helper.rb
+++ app/helpers/tests_helper.rb
@@ -1,4 +1,33 @@
 # frozen_string_literal: true
 
 module TestsHelper
+  EN_LEVELS = {
+    0 => 'very easy',
+    1 => 'easy',
+    2 => 'medium',
+    3 => 'hard',
+    4 => 'very hard',
+    5 => 'brutal'
+  }.freeze
+
+  RU_LEVELS = {
+    0 => 'очень просто',
+    1 => 'просто',
+    2 => 'непросто',
+    3 => 'сложно',
+    4 => 'очень сложно',
+    5 => 'брутально'
+  }.freeze
+
+  def level_in_human_form(test)
+    if I18n.locale == :en
+      return EN_LEVELS[test.level] if EN_LEVELS[test.level]
+
+      'impossible'
+    else
+      return RU_LEVELS[test.level] if RU_LEVELS[test.level]
+
+      'невозможно'
+    end
+  end
 end

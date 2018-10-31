57d1e45983ceb43bee9a6b88bd534bc9bbb98a11
Lokideos <archmage09@gmail.com>
2018-10-31 15:11:48 +0300

    Use locales to determine test level in human form

--- app/helpers/tests_helper.rb
+++ app/helpers/tests_helper.rb
@@ -1,33 +1,17 @@
 # frozen_string_literal: true
 
 module TestsHelper
-  EN_LEVELS = {
-    0 => 'very easy',
-    1 => 'easy',
-    2 => 'medium',
-    3 => 'hard',
-    4 => 'very hard',
-    5 => 'brutal'
-  }.freeze
-
-  RU_LEVELS = {
-    0 => 'очень просто',
-    1 => 'просто',
-    2 => 'непросто',
-    3 => 'сложно',
-    4 => 'очень сложно',
-    5 => 'брутально'
-  }.freeze
-
   def level_in_human_form(test)
-    if I18n.locale == :en
-      return EN_LEVELS[test.level] if EN_LEVELS[test.level]
+    levels = {
+      0 => I18n.t('helper_methods.tests.levels.very_easy'),
+      1 => I18n.t('helper_methods.tests.levels.easy'),
+      2 => I18n.t('helper_methods.tests.levels.medium'),
+      3 => I18n.t('helper_methods.tests.levels.hard'),
+      4 => I18n.t('helper_methods.tests.levels.very_hard'),
+      5 => I18n.t('helper_methods.tests.levels.brutal')
+    }
+    return levels[test.level] if levels[test.level]
 
-      'impossible'
-    else
-      return RU_LEVELS[test.level] if RU_LEVELS[test.level]
-
-      'невозможно'
-    end
+    I18n.t('helper_methods.tests.levels.impossible')
   end
 end

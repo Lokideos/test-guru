6b250a9b38a7bef3137114eb40623b33c6262c41
lokideos <archmage09@gmail.com>
2018-11-03 11:56:02 +0300

    Add foreign_key property to references to question & user in gists migration

--- db/migrate/20181102205051_create_gists.rb
+++ db/migrate/20181102205051_create_gists.rb
@@ -3,8 +3,8 @@
 class CreateGists < ActiveRecord::Migration[5.2]
   def change
     create_table :gists do |t|
-      t.belongs_to :question
-      t.belongs_to :user
+      t.belongs_to :question, foreign_key: true
+      t.belongs_to :user, foreign_key: true
       t.string :url, null: false
 
       t.timestamps

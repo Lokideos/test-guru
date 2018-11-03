750664212801389e70a93cb83caef8aacfaa6604
lokideos <archmage09@gmail.com>
2018-11-03 20:11:56 +0300

    Get rid of redundant URL format check for gists

--- app/models/gist.rb
+++ app/models/gist.rb
@@ -4,13 +4,7 @@ class Gist < ApplicationRecord
   belongs_to :user
   belongs_to :question
 
-  URL_FORMAT = %r{
-    (http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?
-    [a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?
-  }x
-
   validates :url, presence: true
-  validates :url, format: { with: URL_FORMAT }
 
   def hash(size)
     url[size..-1]

2dca5359e1e8b79fb3082b36354a4260ef9d2aab
lokideos <archmage09@gmail.com>
2018-11-12 00:37:41 +0300

    fix mailer settings for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -71,8 +71,8 @@ Rails.application.configure do
     port:                            587,
     user_name:      ENV['SMTP_USERNAME'],
     password:       ENV['SMTP_PASSWORD'],
-    authentication: '             plain',
-    enable_starttls_auto:         true
+    authentication:              'plain',
+    enable_starttls_auto:            true
   }
 
   # Ignore bad email addresses and do not raise email delivery errors.

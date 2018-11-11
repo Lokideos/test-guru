4e66207389335ba1a64c09bc95fec8b54d4c6aa8
lokideos <archmage09@gmail.com>
2018-11-11 23:26:56 +0300

    Fix migration to be able to work in postgresql

--- db/migrate/20181020044837_add_passed_questions_to_test_passages.rb
+++ db/migrate/20181020044837_add_passed_questions_to_test_passages.rb
@@ -3,7 +3,8 @@
 class AddPassedQuestionsToTestPassages < ActiveRecord::Migration[5.2]
   def change
     remove_column :test_passages, :finished, :boolean
-    add_reference :test_passages, :current_question, foreign_key: true
+    add_reference :test_passages, :current_question, index: true
+    add_foreign_key :test_passages, :questions, column: :current_question_id
     add_column :test_passages, :correct_questions, :integer, default: 0
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -12,10 +12,13 @@
 
 ActiveRecord::Schema.define(version: 2018_11_02_205051) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "answers", force: :cascade do |t|
     t.string "body", null: false
     t.boolean "correct", default: false, null: false
-    t.integer "question_id"
+    t.bigint "question_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["question_id"], name: "index_answers_on_question_id"
@@ -28,8 +31,8 @@ ActiveRecord::Schema.define(version: 2018_11_02_205051) do
   end
 
   create_table "gists", force: :cascade do |t|
-    t.integer "question_id"
-    t.integer "user_id"
+    t.bigint "question_id"
+    t.bigint "user_id"
     t.string "url", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -39,18 +42,18 @@ ActiveRecord::Schema.define(version: 2018_11_02_205051) do
 
   create_table "questions", force: :cascade do |t|
     t.string "body", null: false
-    t.integer "test_id"
+    t.bigint "test_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["test_id"], name: "index_questions_on_test_id"
   end
 
   create_table "test_passages", force: :cascade do |t|
-    t.integer "test_id"
-    t.integer "user_id"
+    t.bigint "test_id"
+    t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "current_question_id"
+    t.bigint "current_question_id"
     t.integer "correct_questions", default: 0
     t.index ["current_question_id"], name: "index_test_passages_on_current_question_id"
     t.index ["test_id"], name: "index_test_passages_on_test_id"
@@ -62,8 +65,8 @@ ActiveRecord::Schema.define(version: 2018_11_02_205051) do
     t.integer "level", default: 0, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "category_id"
-    t.integer "author_id"
+    t.bigint "category_id"
+    t.bigint "author_id"
     t.index ["author_id"], name: "index_tests_on_author_id"
     t.index ["category_id"], name: "index_tests_on_category_id"
     t.index ["title", "level"], name: "index_tests_on_title_and_level", unique: true
@@ -94,4 +97,12 @@ ActiveRecord::Schema.define(version: 2018_11_02_205051) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "answers", "questions"
+  add_foreign_key "gists", "questions"
+  add_foreign_key "gists", "users"
+  add_foreign_key "questions", "tests"
+  add_foreign_key "test_passages", "questions", column: "current_question_id"
+  add_foreign_key "test_passages", "tests"
+  add_foreign_key "test_passages", "users"
+  add_foreign_key "tests", "categories"
 end
